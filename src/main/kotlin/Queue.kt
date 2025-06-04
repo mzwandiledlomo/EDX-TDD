3894a9dd1c4f08247ec3952c71589c705c456b17
Mzwandile Dlomo <world.mjdlomo@gmail.com>
2025-06-04 03:01:29 +0200

    failing enqueue test

--- src/main/kotlin/Queue.kt
+++ src/main/kotlin/Queue.kt
@@ -2,5 +2,10 @@ import java.util.*
 
 class Queue<E> {
     private var elements = LinkedList<E>()
+    fun enqueue(e: E) {
+    }
 
+    fun peek(): E {
+        throw NotImplementedError("peek() not implemented")
+    }
 }
--- src/test/kotlin/QueueTest.kt
+++ src/test/kotlin/QueueTest.kt
@@ -1,2 +1,12 @@
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
 class QueueTest {
+
+    @Test
+    fun `enqueue should add elements to the queue`() {
+        val queue = Queue<Int>()
+        queue.enqueue(1)
+        assertEquals(1, queue.peek())
+    }
 }
